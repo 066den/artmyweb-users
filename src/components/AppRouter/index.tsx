850d06878b9ee2be969975d28aee52d10893a308
den <066den@gmail.com>
2022-06-28 14:53:36 +0300

    api url

--- src/components/AppRouter/index.tsx
+++ src/components/AppRouter/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import UserEditPage from '../../pages/UserEditPage';
 import UsersPage from '../../pages/UsersPage';
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path='/users' element={<UsersPage />} />
+      <Route path='/' element={<UsersPage />} />
       <Route path='/edit/:id' element={<UserEditPage />} />
-      <Route path='/' element={<Navigate replace to='/users' />} />
     </Routes>
   );
 };
